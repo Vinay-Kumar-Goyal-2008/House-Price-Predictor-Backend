24eab1b437116a254b30c1e4fcb1a2b61bb78155
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-08-23 20:37:21 +0530

    jsajo

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask,render_template,request,jsonify
 import numpy as np
 import os
 import pickle
+from flask_cors import CORS
+
 app= Flask(__name__)
+CORS(app)
 
 @app.route('/submitval', methods=['POST'])
 def submitval():
