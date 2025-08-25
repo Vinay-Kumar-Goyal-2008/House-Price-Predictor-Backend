85a02a08c58b109a6ceb5b31b9504c644b805ec0
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-08-25 16:52:51 +0530

    New backend created'

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ def submitval():
     if data!={}:
         with open('model.pkl','rb') as f:
             model=pickle.load(f)
-        price= model.predict([[int(data['overallqual']),int(data['livingarea']),int(data['garagecap']),int(data['garagear']),int(data['basear']),int(data['firstar']),int(data['fullbath']),int(data['totrooms']),int(data['yrbuilt']),int(data['rebuilt'])]])
-        price=np.expm1(price)
+        price= model.predict([[int(data['livingarea']),int(data['basear']),int(data['firstar']),int(data['yrbuilt']),int(data['rebuilt'])]])
         return jsonify({'status':200,'desc':'Request recieved and answer given','ans':float(price[0])})
     else:
         return jsonify({'status':404,'desc':'Data not received Try again','ans':'N/A'})
